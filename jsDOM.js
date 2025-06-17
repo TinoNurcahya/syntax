3bd3d6d89f7aef7a0ba775409a3daa9e9e9bccec
TinoNurcahya <tinonurcahya@gmail.com>
2025-06-17 21:12:38 +0700

    DOM tree

--- jsDOM.js
+++ jsDOM.js
@@ -5,3 +5,15 @@ DOM TRAVERSAL (MENELUSURI)
 EVENT HANDLING
 STUDI KASUS
 */
+
+===================== DOM Tree =====================
+root node merupakan sumber dari semua node lain di dalam DOM. Contohnya Document
+
+parent node merupakan node yang berada 1 tingkat diatas node lain. contohnya body adalah parent dari h1, p, div dan a
+
+child node merupakan node yang berada 1 tingkat dibawah node yang lain. contohnya h1 adalah child dari body
+
+document>html>head & body
+
+head>title>Text
+body>semua yang ada di body seperti p, div, h1, dan sebagainya
